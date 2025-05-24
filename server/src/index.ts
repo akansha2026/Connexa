e30522f69096c346824f9a35d7e84e063b3fe90b
akansha2026 <akanshaiitkgp2005@gmail.com>
2025-05-24 18:00:09 +0530

    Initial setup for client & server

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// Load env variables
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Hello from Express + TypeScript!");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server running at http://localhost:${port}`);
+});
