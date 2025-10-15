d5fae85ac98cb2cbd7119404758dabb8382cee49
Akansha <137085539+akansha2026@users.noreply.github.com>
2025-10-16 03:10:32 +0530

    Change email transport to secure mode

--- server/src/configs/email.ts
+++ server/src/configs/email.ts
@@ -4,7 +4,7 @@ import { SMTP_HOST, SMTP_PASS, SMTP_PORT, SMTP_USER } from "./variables";
 export const emailClient = nodemailer.createTransport({
   host: SMTP_HOST,
   port: parseInt(SMTP_PORT!, 10),
-  secure: false, // true for 465, false for other ports
+  secure: true, // true for 465, false for other ports
   auth: {
     user: SMTP_USER!,
     pass: SMTP_PASS || "",
